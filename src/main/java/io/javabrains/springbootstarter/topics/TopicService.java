7e4637ab1105e00700e5ceb4fc51167747ab4f0d
Ted Vu <tedvu184@gmail.com>
2020-09-08 20:25:07 +1000

    Build a complete RESTAPI

--- src/main/java/io/javabrains/springbootstarter/topics/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topics/TopicService.java
@@ -1,6 +1,8 @@
 package io.javabrains.springbootstarter.topics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -8,9 +10,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("java", "Core Java", "Core Java Description"),
-			new Topic("JavaScript", "JavaScript", "JavaScript Description"));
+	private List<Topic> topics = new ArrayList<>(
+			Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
+					new Topic("java", "Core Java", "Core Java Description"),
+					new Topic("JavaScript", "JavaScript", "JavaScript Description")));
 
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -24,5 +27,29 @@ public class TopicService {
 //		}
 //		throw new IllegalArgumentException("");
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		for (int i = 0; i < topics.size(); ++i) {
+			if (topics.get(i).getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		Iterator<Topic> topicIt = topics.iterator();
+		while (topicIt.hasNext()) {
+			if (topicIt.next().getId().equals(id)) {
+				topicIt.remove();
+				return;
+			}
+		}
+
 	}
 }
